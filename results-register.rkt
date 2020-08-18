7e78fa37f3ba5849fc9b7ec791403652b60efdc1
Dan Cascaval <dcascava@andrew.cmu.edu>
2020-08-17 23:51:13 -0400

    timings

--- results-register.rkt
+++ results-register.rkt
@@ -1,4 +1,5 @@
 ; box 2-2
+; time: 4045ms
 (define (synthed-func input0)
   (define r0 input0)
   (define r1 (translateZ+ r0))
@@ -10,7 +11,9 @@
   r6))
 
 ; planes-3. This is expressible with mirrors but would otherwise
-; be impossible with the standard translate functions.
+; be impossible with the standard translate functions
+; (Or rather, it would take a far longer program.)
+; time: 186ms
 (define (synthed-func input0)
   (define r0 input0)
   (define r1 (mirrorX r0))
@@ -18,8 +21,8 @@
   (define r3 (mirrorZ r2))
   r3))
 
-; planes-3-chunk
-; Combines mirror & diff
+; planes-3-chunk. Combines mirror & diff
+; time: 484ms
 (define (synthed-func input0)
   (define r0 input0)
   (define r1 (mirrorX r0))
@@ -29,6 +32,7 @@
   r4))
 
 ; true-mirror-box
+; time: 59833ms (60s)
 (define (synthed-func input0)
   (define r0 input0)
   (define r1 (tY+ r0))
@@ -41,10 +45,27 @@
   r7))
 
 ; unable to effectivly synthesize programs longer than 7 instructions.
-; even 7 is only feasible when there is known to be a solution, otherwise
+; 7-9 is only feasible when there is known to be a solution, otherwise
 ; it can take 12-13x as long to prove that there isn't one.
 ;
 ; unfortunately, the register language is not very expressive, and a depth-7
 ; AST can do much, much more than 7 instructions can (that is up to 2^7 registers,
 ; and the register language only advantages us when the AST subtrees are shared,
 ; which isn't the case with even slightly-irregular result shapes).
+
+; ... but, if we push it, and we know a solution exists, we can do 9.
+
+; full-box
+; time: 4781369ms (about 1h20m)
+(define (synthed-func input0)
+  (define r0 input0)
+  (define r1 (tX+ r0))
+  (define r2 (union r1 r0))
+  (define r3 (tY+ r2))
+  (define r4 (union r2 r3))
+  (define r5 (tZ+ r4))
+  (define r6 (union r4 r5))
+  (define r7 (mirrorX r6))
+  (define r8 (mirrorY r7))
+  (define r9 (mirrorZ r8))
+  r9))
